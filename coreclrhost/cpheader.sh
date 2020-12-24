e0e873d153353b7bc2d173a414e85d4da67e84ab
pg-ito <eit2357@gmail.com>
2020-12-25 07:44:02 +0900

    add ignored files

--- coreclrhost/cpheader.sh
+++ coreclrhost/cpheader.sh
@@ -1,5 +1,12 @@
 #!/bin/bash -xe
 
-cp -fpv coreclrhost/coreclr_ctlpp.h ../lib
+cd $(dirname $0)
 
+LIB_DIR="../lib"
+[ ! -e "${LIB_DIR}" ] && mkdir ${LIB_DIR}
+
+
+cp -fpv coreclr_ctlpp.h ${LIB_DIR}
+
+cd -
 
