a73f9d3a0393306cd4093d30d1c72fe42508a21a
pg-ito <eit2357@gmail.com>
2020-12-26 16:26:35 +0900

    remove comment fix debug log macro

--- coreclrhost/coreclr_ctlpp.h
+++ coreclrhost/coreclr_ctlpp.h
@@ -3,7 +3,7 @@
 
 
 #define DEBUG 0
-#define DOTNET_FFI_DEBUGLOG(fmt, ...) do { if (DEBUG) fprintf(stderr, "DOTNET_FFI_DEBUG %s:%d:%s(): " fmt, __FILE__, __LINE__, __func__, __VA_ARGS__); } while (0)
+#define DOTNET_FFI_DEBUGLOG(fmt, ...) do { if (DEBUG){ fprintf(stderr, "DOTNET_FFI_DEBUG %s:%d:%s(): " fmt, __FILE__, __LINE__, __func__, __VA_ARGS__);} } while (0)
 #define DOTNET_FFI_ERRLOG(fmt, ...) do { fprintf(stderr, "DOTNET_FFI_ERROR! %s:%d:%s(): " fmt, __FILE__, __LINE__, __func__, __VA_ARGS__); } while (0)
 
 #ifdef __cplusplus 
@@ -18,12 +18,6 @@ double InvokeReturnDouble(int *hr, double d);
 void invoke_ret_str_arg_str(int *hr, const char *inStr, int inLen, char **retStr, int *retLen, const char* method_name);
 long long InvokeReturnInt64(int *hr, long long i, long long j);
 int DestructVm();
-
-#ifdef DOTNET_FFI_LOG_ENBALE
-#define DBGLOG( debuglog ) printf( "File:%s Line:%d Func:%s Log:%s\n", __FILE__, __LINE__, __func__, debuglog )
-#else
-#define DBGLOG( debuglog )
-#endif
 
 #ifdef __cplusplus 
 } //end extern "C" 
