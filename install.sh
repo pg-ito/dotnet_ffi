ab6568fafacf3d16dc421eafc9c4094d88acd3b9
pg-ito <eit2357@gmail.com>
2020-12-31 00:07:02 +0900

    fix install.sh for apache2 suport dotnet5.0

--- install.sh
+++ install.sh
@@ -5,18 +5,22 @@ LIBCORECLR_CTL_DIR=${LIBCORECLR_CTL_DIR:-/usr/local/lib/}
 
 # e.g. export EXTENSION_DIR=/usr/local/lsws/lsphp7_4_12/lib/php/extensions/no-debug-non-zts-20190902/
 echo ${EXTENSION_DIR}
-EXTENSION_DIR=${EXTENSION_DIR:-/usr/local/lib/php/extensions/}
+EXTENSION_DIR=${EXTENSION_DIR:-/usr/lib/php/20190902/}
 
 # e.g. export PHP_INI_FPATH=/usr/local/lsws/lsphp7_4_12/lib/php.ini
-PHP_INI_FPATH=${PHP_INI_FPATH:-/etc/php/php.ini}
+PHP_INI_FPATH=${PHP_INI_FPATH:-/etc/php/7.4/cli/php.ini}
 echo ${PHP_INI_FPATH}
+
+# e.g. export PHP_ADDITIONAL_INI_DIR=/etc/php/7.4/apache2/conf.d 
+PHP_ADDITIONAL_INI_DIR=${PHP_ADDITIONAL_INI_DIR:-/etc/php/7.4/apache2/conf.d/}
+echo ${PHP_ADDITIONAL_INI_DIR}
 
 DOTNET_FFI_INI_FILE=dotnet_ffi.ini
 cat << EOM
-LIBCORECLR_CTL_DIR  ${LIBCORECLR_CTL_DIR}
-EXTENSION_DIR       ${EXTENSION_DIR}
-PHP_INI_FPATH       ${PHP_INI_FPATH}
-
+LIBCORECLR_CTL_DIR      ${LIBCORECLR_CTL_DIR}
+EXTENSION_DIR           ${EXTENSION_DIR}
+PHP_INI_FPATH           ${PHP_INI_FPATH}
+PHP_ADDITIONAL_INI_DIR  ${PHP_ADDITIONAL_INI_DIR}
 ========== ${DOTNET_FFI_INI_FILE} ===========
 $(cat ${DOTNET_FFI_INI_FILE})
 EOM
@@ -27,11 +31,12 @@ sudo cp -ipav coreclrhost ${LIBCORECLR_CTL_DIR}
 sudo cp -ipav modules/dotnet_ffi.so ${EXTENSION_DIR}
 
 
-if [ $(grep '^\[dotnet_ffi\]' ${PHP_INI_FPATH}|wc -l) -eq 0 ];then
-  sudo bash -c "cat dotnet_ffi.ini >> ${PHP_INI_FPATH}"
-  echo -e "added dotnet_ffi config to ${INI_CONF_EXISTS}\n"
-fi
+# if [ $(grep '^\[dotnet_ffi\]' ${PHP_INI_FPATH}|wc -l) -eq 0 ];then
+#   sudo bash -c "cat dotnet_ffi.ini >> ${PHP_INI_FPATH}"
+#  echo -e "added dotnet_ffi config to ${INI_CONF_EXISTS}\n"
+# fi
 
+sudo cp -ipav ${DOTNET_FFI_INI_FILE} ${PHP_ADDITIONAL_INI_DIR}
 
 echo -e "extension installed. Need reboot your httpd server.\n"
 
