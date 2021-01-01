77b4d47c9107a1fb34d5af9fd87970cd09867372
pg-ito <eit2357@gmail.com>
2021-01-01 18:33:30 +0900

    fix build process in br.sh

--- br.sh
+++ br.sh
@@ -4,17 +4,18 @@ cd $(dirname $0)
 export PHP_PATH=${PHP_PATH:-/usr/bin/}
 echo build and run
 
+OPTS=$1
 
 coreclrhost/cpheader.sh
 # /usr/local/php7_2_24dev/bin/phpize
-${PHP_PATH}phpize
+${PHP_PATH}phpize ${OPTS}
 ./configure --enable-dotnet_ffi -with-php-config=${PHP_PATH}/php-config
 make clean 
 dotnet_dll/dotnet_publish.sh invokee_test
 # dotnet_dll/dotnet_publish.sh fibonacci
 coreclrhost/buildlib.sh
 
-make -j6 -d --trace 
+make -j6 
 # ldd modules/dotnet_ffi.so
 # nm modules/dotnet_ffi.so
 
